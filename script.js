84bb89d7a9712df2d180de56c2ccef85d5731ade
nani <dms52503@gmail.com>
2026-01-02 17:35:45 +0530

    Initial loan prediction app using nano

---
+++ script.js
@@ -0,0 +1,28 @@
+function login() {
+  let e = email.value;
+  let p = password.value;
+
+  if (e === "narnenani99@gmail.com" && p === "nani0909") {
+    location.href = "loan.html";
+  } else {
+    msg.innerText = "Unauthorized Access";
+  }
+}
+
+function predictLoan() {
+  let i = +income.value;
+  let l = +loan.value;
+  let c = +credit.value;
+
+  let h = JSON.parse(localStorage.getItem("history")) || [];
+
+  if (i >= 50000 && c >= 720) {
+    result.innerText = "Loan Approved";
+    h.push("Approved Loan: " + l);
+  } else {
+    result.innerText = "Loan Rejected";
+    h.push("Rejected Loan: " + l);
+  }
+
+  localStorage.setItem("history", JSON.stringify(h));
+}
